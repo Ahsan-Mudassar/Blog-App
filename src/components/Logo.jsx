d2640aa9b27634bea88ee0fce9a9e08f7800beff
Ahsan-Mudassar <ahsanmudassar441@gmail.com>
2026-04-18 20:57:08 +0500

    fixed logo error

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import logo from '../assets/logo.png'
 
-function Logo({width}) {
+function Logo() {
   return (
     <div className='text-3xl font-semibold flex'>
-      <img className='h-10' src='src/assets/logo.png'alt="logo" />
+      <img className='h-10' src={logo} alt="logo" />
       Blog 
       <span className='text-indigo-900 text-2xl font-bold'>App</span>
     </div>
